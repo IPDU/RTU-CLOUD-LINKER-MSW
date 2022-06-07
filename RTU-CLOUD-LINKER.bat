0129bbc3d457d4e3736b5bf40da08b137b28a150
kroko <720976+kroko@users.noreply.github.com>
2022-06-07 19:44:19 +0300

    add script

---
+++ RTU-CLOUD-LINKER.bat
@@ -0,0 +1,14 @@
+:: this script creates symbolic links under \IPIN_RTUCLOUD4
+:: for all channels user has synced from RTU CLOUD
+:: must be run as administrator because of MKLINK
+:: see README
+
+@ECHO OFF
+
+SET "DIRNAME_FOR_LINKS=IPIN_RTUCLOUD1"
+
+RMDIR /S /Q "%HOMEDRIVE%\%DIRNAME_FOR_LINKS%"
+MKDIR "%HOMEDRIVE%\%DIRNAME_FOR_LINKS%"
+FOR /F "delims=" %%D IN ('DIR /ad /b  %HOMEDRIVE%%HOMEPATH%\rtucloud1\') DO (
+  MKLINK /d "%HOMEDRIVE%\%DIRNAME_FOR_LINKS%\%%D" "%HOMEDRIVE%%HOMEPATH%\rtucloud1\%%D"
+)
